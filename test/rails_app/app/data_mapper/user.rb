a523a7f7ea2502444889402cd1be06ff6495f29a
Jared Morgan <jmorgan@morgancreative.net>
2010-09-08 19:56:37 -0500

    Remove condition from User test model for data_mapper
    Had unnecessarily verified that DEVISE_ORM is not data_mapper_active_model

--- test/rails_app/app/data_mapper/user.rb
+++ test/rails_app/app/data_mapper/user.rb
@@ -11,17 +11,15 @@ class User
   include SharedUser
   include Shim
 
-  unless DEVISE_ORM == :data_mapper_active_model
-    before :valid?, :update_password_confirmation
+  before :valid?, :update_password_confirmation
 
-    # DM's validates_confirmation_of requires the confirmation field to be present,
-    # while ActiveModel by default skips the confirmation test if the confirmation
-    # value is nil. This test takes advantage of AM's behavior, so just add the
-    # :password_confirmation value.
-    def update_password_confirmation
-      if self.password && self.password_confirmation.nil?
-        self.password_confirmation = self.password
-      end
+  # DM's validates_confirmation_of requires the confirmation field to be present,
+  # while ActiveModel by default skips the confirmation test if the confirmation
+  # value is nil. This test takes advantage of AM's behavior, so just add the
+  # :password_confirmation value.
+  def update_password_confirmation
+    if self.password && self.password_confirmation.nil?
+      self.password_confirmation = self.password
     end
   end 
 end
