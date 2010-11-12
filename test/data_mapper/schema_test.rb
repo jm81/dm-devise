2b29553347aad1c18a8f9ac29c971b789556d6f9
Jared Morgan <jmorgan@morgancreative.net>
2010-11-12 12:10:54 -0600

    Add test for explicitly setting required option to false in apply_schema
    Not sure if this is the best way to test it, but seems to work.

---
+++ test/data_mapper/schema_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class DataMapperSchemaTest < ActiveSupport::TestCase
+  test 'required explicitly set to false' do
+    DataMapper::Property.required(true)
+    model = Class.new(User)
+    model.apply_devise_schema :required_string, String, :required => true
+    model.apply_devise_schema :not_required_string, String
+    assert model.properties['required_string'].required?
+    assert !model.properties['not_required_string'].required?
+    DataMapper::Property.required(false)
+  end
+end
