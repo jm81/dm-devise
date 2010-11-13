eaec3e3b30f0c102872708c8b9f5f8817c20fd9b
Jared Morgan <jmorgan@morgancreative.net>
2010-11-12 19:21:26 -0600

    Ensure DataMapper::Property.required is set back to false in schema test

--- test/data_mapper/schema_test.rb
+++ test/data_mapper/schema_test.rb
@@ -2,12 +2,15 @@ require 'test_helper'
 
 class DataMapperSchemaTest < ActiveSupport::TestCase
   test 'required explicitly set to false' do
-    DataMapper::Property.required(true)
-    model = Class.new(User)
-    model.apply_devise_schema :required_string, String, :required => true
-    model.apply_devise_schema :not_required_string, String
-    assert model.properties['required_string'].required?
-    assert !model.properties['not_required_string'].required?
-    DataMapper::Property.required(false)
+    begin
+      DataMapper::Property.required(true)
+      model = Class.new(User)
+      model.apply_devise_schema :required_string, String, :required => true
+      model.apply_devise_schema :not_required_string, String
+      assert model.properties['required_string'].required?
+      assert !model.properties['not_required_string'].required?
+    ensure
+      DataMapper::Property.required(false)
+    end
   end
 end
