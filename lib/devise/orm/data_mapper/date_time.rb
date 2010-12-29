8112ccfae585f996de20c5bf5276a9a2f024347a
Jared Morgan <jmorgan@morgancreative.net>
2010-12-28 21:17:09 -0600

    Change DateTime#gmtime to use #new_offset instead of delegating to #to_time
    #to_time returns self if the TZ offset is not 0, causing a loop.
    
    This might fix GH-7, but I can't seem to get the offset to not be 0 so I
    can't reproduce.

--- lib/devise/orm/data_mapper/date_time.rb
+++ lib/devise/orm/data_mapper/date_time.rb
@@ -1,6 +1,8 @@
 # Add DateTime#gmtime method if needed.
 class DateTime
   unless method_defined?(:gmtime)
-    delegate :gmtime, :to => :to_time
+    def gmtime
+      self.new_offset(0)
+    end
   end
 end
