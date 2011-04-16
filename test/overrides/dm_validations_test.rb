d09b73bb97435ee732f5375e5ee80ef8b8939910
Jared Morgan <jmorgan@morgancreative.net>
2011-04-16 14:05:24 -0500

    Update test overrides for maximum password length of 128
    plataformatec/devise@8db00eed

--- test/overrides/dm_validations_test.rb
+++ test/overrides/dm_validations_test.rb
@@ -12,17 +12,17 @@ if DEVISE_ORM == :data_mapper
       user = new_user(:password => '12345', :password_confirmation => '12345')
       assert user.invalid?
       # assert_equal 'is too short (minimum is 6 characters)', user.errors[:password].join
-      assert_equal 'must be between 6 and 20 characters long', user.errors[:password].join
+      assert_equal 'must be between 6 and 128 characters long', user.errors[:password].join
     end
 
-    undef test_should_require_a_password_with_maximum_of_20_characters_long
+    undef test_should_require_a_password_with_maximum_of_128_characters_long
 
     # Same issue as previous test
-    test 'should require a password with maximum of 20 characters long' do
-      user = new_user(:password => 'x'*21, :password_confirmation => 'x'*21)
+    test 'should require a password with maximum of 128 characters long' do
+      user = new_user(:password => 'x'*129, :password_confirmation => 'x'*129)
       assert user.invalid?
       # assert_equal 'is too long (maximum is 20 characters)', user.errors[:password].join
-      assert_equal 'must be between 6 and 20 characters long', user.errors[:password].join
+      assert_equal 'must be between 6 and 128 characters long', user.errors[:password].join
     end
 
   end
@@ -40,16 +40,6 @@ if DEVISE_ORM == :data_mapper
         # post user_registration_path(:format => 'xml', :user => {:email => "test@example.com", :password => "invalid"} )
         post user_registration_path(:format => 'xml', :user => {:email => "test@example.com", :password => "invalid", :password_confirmation => "invalid"} )
       end
-    end
-  end
-
-  class ActiveRecordTest < ActiveSupport::TestCase
-    undef test_validations_options_are_not_applied_to_late
-
-    test 'validations options are not applied to late' do
-      validators = WithValidation.validators.contexts[:default].select{|validator| validator.field_name == :password}
-      length = validators.find { |v| v.class.name == 'DataMapper::Validations::LengthValidator' }
-      assert_equal (2..6), length.options[:within]
     end
   end
 end
