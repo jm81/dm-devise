7e407ae6903fb914064b26aad009409074bc57fe
Jared Morgan <jmorgan@morgancreative.net>
2011-08-03 19:58:15 -0500

    DeviseGenerator#replace_default_devise_orm is no longer needed
    Devise inserts the generator ORM.

--- lib/generators/data_mapper/devise_generator.rb
+++ lib/generators/data_mapper/devise_generator.rb
@@ -1,13 +1,4 @@
 require 'generators/devise/orm_helpers'
-require 'sugar-high/file'
-
-# Reload Active Support's String#blank?, overridden by sugar-high. Active Support's
-# version considers any all-space String blank, behavior that devise expects.
-class String #:nodoc:
-  def blank?
-    self !~ /\S/
-  end
-end
 
 module DataMapper
   module Generators
@@ -20,11 +11,6 @@ module DataMapper
 
       def inject_devise_content
         inject_into_file model_path, model_contents, :after => "include DataMapper::Resource\n" if model_exists?
-      end
-
-      def replace_default_devise_orm
-        devise_init_file = File.join(Rails.root, 'config', 'initializers', 'devise.rb')
-        File.replace_content_from devise_init_file, :where => 'orm/active_record', :with => 'orm/data_mapper'
       end
     end
   end
